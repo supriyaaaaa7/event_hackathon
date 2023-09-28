1086175ed82e75a08eee4875771f5d7ac830b13d
yash <101994806+yash-rajarora@users.noreply.github.com>
2023-09-28 09:55:16 +0530

    final

--- lib/screens/User_Home/MyTicket.dart
+++ lib/screens/User_Home/MyTicket.dart
@@ -3,6 +3,7 @@ import 'package:event/components/already_have_an_account_acheck.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'components/Registered card.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 
 Widget divider() {
@@ -17,26 +18,31 @@ Widget divider() {
 class MyTicket extends StatefulWidget {
   const MyTicket({super.key});
 
+
   @override
   State<MyTicket> createState() => _MyTicketState();
 }
 
 class _MyTicketState extends State<MyTicket> {
-  String userEventname = "";
-  String userEmailid = "";
+  List<UserData> userDataList = [];
 
   @override
   void initState() {
     super.initState();
-    fetchUserRole(); // Call the function to fetch the user role when the widget is created
+    fetchUserList(); // Call the function to fetch user data when the widget is created
   }
 
-  Future<void> fetchUserRole() async {
+  Future<void> fetchUserList() async {
     final user = FirebaseAuth.instance.currentUser;
-    final userDoc = await FirebaseFirestore.instance.collection('data').doc(user?.uid).get();
+    final uid = user?.uid;
+    final userDocs = await FirebaseFirestore.instance.collection(uid!).get();
     setState(() {
-      userEventname = userDoc.data()?['Event Name'] ?? "";
-      userEmailid = userDoc.data()?['Email'] ?? "";
+      userDataList = userDocs.docs.map((doc) {
+        return UserData(
+          eventName: doc.data()?['Event Name'] ?? "",
+          email: doc.data()?['Email'] ?? "",
+        );
+      }).toList();
     });
   }
 
@@ -48,7 +54,7 @@ class _MyTicketState extends State<MyTicket> {
           SizedBox(height: 25),
           Container(
             padding: EdgeInsets.only(left: 25),
-            child: Text("Registered Events", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900,color: Colors.black),),
+            child: Text("Registered Events", style: TextStyle(fontSize: 30, fontWeight: FontWeight.w900, color: Colors.black)),
           ),
           divider(),
           Padding(
@@ -56,11 +62,47 @@ class _MyTicketState extends State<MyTicket> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                OrderContainer(
-                  productName: userEventname,
-                  email: userEmailid,
-                  orderStatus: 'Confirmed',
-                )
+                ...userDataList.map((userData) {
+                  return OrderContainer(
+                    productName: userData.eventName,
+                    email: userData.email,
+                    orderStatus: 'Confirmed',
+                  );
+                }).toList(),
+                SizedBox(height: 16), // Add some spacing between the OrderContainers and the button
+                ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('QR Code Ticket'),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              QrImageView(
+                                data: '1234567890',
+                                version: QrVersions.auto,
+                                size: 200.0,
+                              ),
+                              SizedBox(height: 16.0),
+                              Text('Scan this QR code to access your ticket.'),
+                            ],
+                          ),
+                          actions: [
+                            ElevatedButton(
+                              onPressed: () {
+                                Navigator.of(context).pop(); // Close the dialog
+                              },
+                              child: Text('Close'),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Text("Your Button Text"),
+                ),
               ],
             ),
           ),
@@ -69,3 +111,14 @@ class _MyTicketState extends State<MyTicket> {
     );
   }
 }
+
+class UserData {
+  final String eventName;
+  final String email;
+
+  UserData({
+    required this.eventName,
+    required this.email,
+  });
+}
+
